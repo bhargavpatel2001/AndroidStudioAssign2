4eb7ffa14ed9a34860e3ce58363b7d5a8b23d8a5
bhargavPatel3029 <N01373029@humbermail.ca>
2021-02-18 16:46:47 -0500

    Finished setting up the file and about to start the code editing

---
+++ app/src/main/java/bhargav/patel/n01373029/BhargavActivity.java
@@ -0,0 +1,16 @@
+//Bhargav Patel N01373029 SectionB
+
+package bhargav.patel.n01373029;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BhargavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
